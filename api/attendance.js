4a92c948a68673502b51844076dee5f0c443a603
박준형 <108733795+krapli441@users.noreply.github.com>
2024-06-05 10:27:43 +0900

    포트 추가

--- api/attendance.js
+++ api/attendance.js
@@ -49,4 +49,9 @@ app.post("/api/attendance", (req, res) => {
   }
 });
 
+const PORT = process.env.EXPRESS_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
