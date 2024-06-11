3432d6a06f6ba02980e7f49c08231928534071b4
박준형 <108733795+krapli441@users.noreply.github.com>
2024-06-11 16:59:17 +0900

    채널 조회 본문 전송하는 코드 작성

--- api/slackChannelId.js
+++ api/slackChannelId.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const slackToken = process.env.SLACK_BOT_TOKEN;
 
 router.post("/", async (req, res) => {
-  const { channel_name, response_url } = req.body;
+  const { response_url } = req.body;
 
   try {
     const response = await axios.get(
@@ -18,42 +18,22 @@ router.post("/", async (req, res) => {
       }
     );
 
-    const channels = response.data.channels;
-    const channel = channels.find((ch) => ch.name === channel_name);
+    const responseText = JSON.stringify(response.data, null, 2);
 
-    if (channel) {
-      const responseText = `채널 이름: ${channel_name}\n채널 ID: ${channel.id}`;
-      await axios.post(
-        response_url,
-        {
-          text: responseText,
-          response_type: "in_channel",
+    await axios.post(
+      response_url,
+      {
+        text: `응답 본문:\n\`\`\`${responseText}\`\`\``,
+        response_type: "in_channel",
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${slackToken}`,
         },
-        {
-          headers: {
-            Authorization: `Bearer ${slackToken}`,
-          },
-        }
-      );
+      }
+    );
 
-      res.status(200).send();
-    } else {
-      const responseText = `채널 이름 ${channel_name}을 찾을 수 없습니다.`;
-      await axios.post(
-        response_url,
-        {
-          text: responseText,
-          response_type: "ephemeral",
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${slackToken}`,
-          },
-        }
-      );
-
-      res.status(404).send();
-    }
+    res.status(200).send();
   } catch (error) {
     console.error("Error fetching channel list:", error.message);
     await axios.post(
