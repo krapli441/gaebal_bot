c83773afdd5cdecae801efb08fdcdcec10072b81
박준형 <108733795+krapli441@users.noreply.github.com>
2024-06-11 13:03:04 +0900

    리팩토링 수정

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 require("./config/firebase.js");
 
-const attendanceRoutes = require("./routes/attendance");
+const attendanceRoutes = require("./api/attendance");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
