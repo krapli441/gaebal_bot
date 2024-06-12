3a1c7bda1da51c881c08511308fe6f849e23a3de
박준형 <108733795+krapli441@users.noreply.github.com>
2024-06-12 15:52:26 +0900

    호출 방식 변경하고 다시 시도

--- api/scheduler.js
+++ api/scheduler.js
@@ -1,16 +1,20 @@
+const express = require("express");
 const { sendSlackChannelMessage } = require("../utils/slack");
 
-module.exports = async (req, res) => {
-  const slackChannelId = "C03QRB7635X"; // Slack 채널 ID
-  const slackToken = process.env.SLACK_BOT_TOKEN;
+const router = express.Router();
+const slackChannelId = "C03QRB7635X";
+const slackToken = process.env.SLACK_BOT_TOKEN;
 
+router.post("/", async (req, res) => {
   try {
-    const message = "테스트 메세지 전송~";
+    const message = "This is a test message sent manually.";
     await sendSlackChannelMessage(slackChannelId, message, slackToken);
-    console.log("Message sent to Slack channel.");
-    res.status(200).send("Message sent to Slack channel.");
+    console.log("Manual message sent to Slack channel.");
+    res.status(200).send("Manual message sent to Slack channel.");
   } catch (error) {
-    console.error("Error sending message to Slack:", error.message);
-    res.status(500).send("Error sending message to Slack.");
+    console.error("Error sending manual message to Slack:", error.message);
+    res.status(500).send("Error sending manual message to Slack.");
   }
-};
+});
+
+module.exports = router;
