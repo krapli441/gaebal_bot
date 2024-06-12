6990aac1db3f97a03f095509236157aebb398822
박준형 <108733795+krapli441@users.noreply.github.com>
2024-06-12 15:07:33 +0900

    app.js 및 vercel.json에 라우트 추가

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ require("dotenv").config();
 require("./config/firebase.js");
 
 const attendanceRoutes = require("./api/attendance");
+const schedulerRoutes = require("./api/scheduler");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/api/attendance", attendanceRoutes);
+app.use("/api/scheduler", schedulerRoutes);
 
 module.exports = app;
