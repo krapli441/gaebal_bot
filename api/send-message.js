035e14d9d6bd1fbd9d4ae8a37a8684c72b7ac595
박준형 <108733795+krapli441@users.noreply.github.com>
2024-06-13 13:09:52 +0900

    함수 수정

--- api/send-message.js
+++ api/send-message.js
@@ -1,13 +1,16 @@
-const { sendSlackMessage } = require("../utils/slack");
+const { sendSlackChannelMessage } = require("../utils/slack");
 
 module.exports = async (req, res) => {
-  const channel = "C03QRB7635X"; // 슬랙 채널 ID
+  const channel = "#your-channel-id"; // 슬랙 채널 ID
   const message = "This is a test message sent every minute.";
   try {
-    await sendSlackMessage(channel, message);
+    await sendSlackChannelMessage(channel, message);
     return res.status(200).json({ message: "Message sent successfully." });
   } catch (error) {
-    console.error("Error sending message to Slack channel:", error.message);
-    return res.status(500).json({ error: "Failed to send message." });
+    console.error("Error sending message to Slack channel:", error); // 에러 객체 전체를 로그로 출력
+    return res.status(500).json({
+      error: `Failed to send message: ${error.message}`,
+      details: error,
+    });
   }
 };
