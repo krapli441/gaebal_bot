f43f142f47a3dbf7fcbf8a63a2ac14cac0b36f8e
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-08 14:32:14 +0900

    0명으로 나옴

--- api/gatherTownCheck.js
+++ api/gatherTownCheck.js
@@ -1,6 +1,8 @@
 const { Game } = require("@gathertown/gather-game-client");
 const { sendSlackMessage } = require("../utils/slack");
 
+require("dotenv").config();
+
 const apiKey = process.env.GATHER_API_KEY;
 const spaceId = process.env.GATHER_SPACE_ID;
 
@@ -11,14 +13,34 @@ const gatherTownCheck = async (req, res) => {
   game.connect();
 
   try {
+    // 연결 이벤트 대기
     await new Promise((resolve) => game.subscribeToConnection(resolve));
 
-    const userCount = Object.keys(game.players).length;
-    game.disconnect();
+    // playerJoins 이벤트 구독
+    game.subscribeToEvent("playerJoins", () => {
+      const userCount = Object.keys(game.players).length;
+      const responseText = `현재 게더 타운 접속자 수: ${userCount}명`;
+      
+      // Slack 메시지 전송
+      sendSlackMessage(response_url, responseText, "in_channel")
+        .then(() => res.status(200).send())
+        .catch((error) => {
+          console.error("Error sending Slack message:", error);
+          res.status(500).json({
+            response_type: "ephemeral",
+            text: `명령어 호출 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요. 에러: ${error.message}`,
+            details: error.message,
+          });
+        });
+    });
 
+    // 기존 플레이어 정보 가져오기
+    const userCount = Object.keys(game.players).length;
     const responseText = `현재 게더 타운 접속자 수: ${userCount}명`;
 
+    // Slack 메시지 전송
     await sendSlackMessage(response_url, responseText, "in_channel");
+    game.disconnect();
     res.status(200).send();
   } catch (error) {
     console.error("Error checking Gather Town users:", error);
