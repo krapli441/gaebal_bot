fb23665d27ee282ea713187fc5daf29a660d2ac0
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-09 14:39:50 +0900

    우선 잘 되는지부터 테스트 필여

--- api/attendance.js
+++ api/attendance.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const checkIn = require("./attendanceCheckIn");
 const checkOut = require("./attendanceCheckOut");
+const checkHistory = require("./checkHistory");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.post("/api/attendance/checkIn", checkIn);
 app.post("/api/attendance/checkOut", checkOut);
+app.post("/api/attendance/checkHistory", checkHistory);
 
 module.exports = app;
--- api/checkHistory.js
+++ api/checkHistory.js
@@ -1,45 +1,29 @@
 const axios = require("axios");
 const kv = require("../config/kv");
-const { getUserInfo } = require("../utils/slack");
 const { sendSlackMessage } = require("../utils/slack");
 
 const checkHistory = async (req, res) => {
-  const { user_id, response_url } = req.body;
-  const slackToken = process.env.SLACK_BOT_TOKEN;
+  const { response_url } = req.body;
 
   try {
-    // 사용자 정보 가져오기
-    const userProfile = await getUserInfo(user_id, slackToken);
-    const displayName = userProfile.profile.display_name;
-
-    // KV Store에서 사용자 이력 조회
+    // KV Store의 모든 키를 조회
     const keysResponse = await kv.keys("*");
-    const userKeys = keysResponse.filter((key) => key.includes(user_id));
+    const keys = keysResponse.keys; // keysResponse에서 실제 키 목록 추출
 
-    let userHistory = [];
-
-    for (const key of userKeys) {
-      const userData = await kv.hgetall(key);
-      userHistory.push(userData);
-    }
-
-    // 사용자 이력을 텍스트로 변환
-    let historyText = `사용자 <@${user_id}>의 출퇴근 이력:\n`;
-    userHistory.forEach((data, index) => {
-      historyText += `${index + 1}. 날짜: ${data.date}, 출근 시간: ${
-        data.checkIn
-      }, 퇴근 시간: ${data.checkOut}, 작업 시간: ${data.workDuration}\n`;
+    let responseText = "KV Store에 저장된 키 목록:\n";
+    keys.forEach((key, index) => {
+      responseText += `${index + 1}. ${key}\n`;
     });
 
-    if (userHistory.length === 0) {
-      historyText = `사용자 <@${user_id}>의 저장된 이력이 없습니다.`;
+    if (keys.length === 0) {
+      responseText = "KV Store에 저장된 키가 없습니다.";
     }
 
     // 슬랙 메시지 전송
-    await sendSlackMessage(response_url, historyText, "in_channel");
+    await sendSlackMessage(response_url, responseText, "in_channel");
     res.status(200).send();
   } catch (error) {
-    console.error("Error processing check-history command:", error);
+    console.error("Error fetching keys from KV Store:", error);
     res.status(500).json({
       response_type: "ephemeral",
       text: `명령어 호출 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요. 에러: ${error.message}`,
